8e3a04742f03a7f874ce50611614111f7d62ae5b
Sunday Ochuko <sundayochuko101@gmail.com>
2022-10-06 12:50:40 +0300

    deleted unused file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+  return <div className="App">Home</div>;
+};
 
 export default App;
